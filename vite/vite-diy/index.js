55f49a759d5029df1467f0a22cbb2da691a04904
youxingzhi <xingzhi.you@shopee.com>
2021-06-18 18:16:54 +0800

    vite diy

--- vite/vite-diy/index.js
+++ vite/vite-diy/index.js
@@ -1,6 +1,8 @@
 const Koa = require('koa')
 const fs = require('fs')
 const path = require('path')
+const compilerSfc = require('@vue/compiler-sfc')
+const compilerDom = require('@vue/compiler-dom')
 
 const app = new Koa()
 
@@ -40,6 +42,46 @@ app.use(async (ctx) => {
     ctx.type = 'application/javascript'
     content = fs.readFileSync(p, 'utf-8')
     content = rewriteImport(content)
+  } else if (url.endsWith('.css')) {
+    const p = path.resolve(__dirname, url.slice(1))
+    const file = fs.readFileSync(p, 'utf-8')
+    content = `
+      const css = "${file.replace(/\n/g, '')}"
+      let link = document.createElement('style')
+      link.setAttribute('type', 'text/css')
+      debugger
+      document.head.appendChild(link)
+      link.innerHTML = css
+      export default css
+    `
+    ctx.type = 'application/javascript'
+  } else if (url.indexOf('.vue') > -1) {
+    const p = path.resolve(__dirname, url.split('?')[0].slice(1))
+    const {descriptor} = compilerSfc.parse(fs.readFileSync(p, 'utf-8'))
+
+    if (!query.type) {
+      ctx.type = 'application/javascript'
+      ctx.body = `
+        ${rewriteImport(
+          descriptor.script.content.replace(
+            'export default ',
+            'const __script ='
+          )
+        )}
+        import { render as __render } from "${url}?type=template"
+        __script.render = __render
+        export default __script
+      `
+    } else if (query.type === 'template') {
+      const template = descriptor.template
+      const render = compilerDom.compile(template.content, {
+        mode: 'module',
+      }).code
+      ctx.type = 'application/javascript'
+      ctx.body = rewriteImport(render)
+    }
+    console.log(ctx.body)
+    return
   }
   ctx.body = content
 })
--- vite/vite-diy/src/App.vue
+++ vite/vite-diy/src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <div>{{ $store.state.count }}</div>
-    <button @click="$store.commit('increment')">+</button>
+    <span>count is {{ count }} * 2 = {{ double }}</span>
+    <button @click="count++">+</button>
   </div>
 </template>
+
+<script>
+import {computed, ref} from 'vue'
+import {defineComponent} from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const count = ref(6)
+    function add() {
+      count.value++
+    }
+    const double = computed(() => count.value * 2)
+    return {count, add, double}
+  },
+})
+</script>
