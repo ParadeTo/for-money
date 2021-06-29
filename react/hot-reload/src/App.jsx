c1412b7fa346baa813751b0dfb5b031013a3918e
youxingzhi <xingzhi.you@shopee.com>
2021-06-29 18:45:19 +0800

    add hmr demo

--- react/hot-reload/src/App.jsx
+++ react/hot-reload/src/App.jsx
@@ -4,7 +4,7 @@ import {hot} from 'react-hot-loader/root'
 const App = () => {
   const [count, setCount] = useState(0)
   return <>
-  <div onClick={() => setCount(count + 1)}>App2 {count}</div>
+  <div onClick={() => setCount(count + 1)}>A3pp2 {count}</div>
   <span>{count}</span>
   </>
 }
---
+++ webpack/webpack-hmr/src/content.js
@@ -0,0 +1,4 @@
+var ret = 'Hello Webpac2k Hot Module Replacement3 33'
+
+// module.exports = ret
+export default ret
