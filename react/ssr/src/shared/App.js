7dc477d0630a7b7e19655d6cb89a1785fd5b0c90
youxingzhi <xingzhi.you@shopee.com>
2021-07-21 17:27:40 +0800

    draw ellipse in webgl

--- react/ssr/src/shared/App.js
+++ react/ssr/src/shared/App.js
@@ -1,29 +1,36 @@
-import React, { Component } from 'react';
-import Grid from './Grid';
-import routes from './routes';
-import { Router, Switch, Route } from 'react-router-dom';
-import NoMatch from '../shared/NoMatch';
-import NavBar from '../shared/NavBar';
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1> Popular repos by language</h1>
-        <NavBar />
-        <Switch>
-          {routes.map(({ path, exact, component: C, ...rest }) => (
-            <Route
-              key={path}
-              path={path}
-              exact={exact}
-              render={props => <C {...props} {...rest} />}
-            />
-          ))}
-          <Route render={props => <NoMatch {...props} />} />
-        </Switch>
-      </div>
-    );
-  }
+import React, { Component, useState } from 'react';
+// import Grid from './Grid';
+// import routes from './routes';
+// import { Router, Switch, Route } from 'react-router-dom';
+// import NoMatch from '../shared/NoMatch';
+// import NavBar from '../shared/NavBar';
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1> Popular repos by language</h1>
+//         <NavBar />
+//         <Switch>
+//           {routes.map(({ path, exact, component: C, ...rest }) => (
+//             <Route
+//               key={path}
+//               path={path}
+//               exact={exact}
+//               render={props => <C {...props} {...rest} />}
+//             />
+//           ))}
+//           <Route render={props => <NoMatch {...props} />} />
+//         </Switch>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [name, setName] = useState('a')
+  return <div onClick={() => setName('b')}>
+    <span>{name}</span>
+  </div>
 }
 
 export default App;
--- react/ssr/src/shared/index.js
+++ react/ssr/src/shared/index.js
@@ -2,12 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import { renderToString } from 'react-dom/server';
 import React from 'react';
-import App from '../shared/App';
+import App from './App';
 import serialize from 'serialize-javascript';
 import { matchPath, StaticRouter } from 'react-router-dom';
-import routes from '../shared/routes';
+import routes from './routes';
 import paths from '../../config/paths';
-import { configureStore } from '../shared/store';
+import { configureStore } from './store';
 import { Provider } from 'react-redux';
 const app = express();
 global.testLog = function () {
@@ -18,18 +18,41 @@ app.use(cors());
 app.use(express.static(paths.clientBuild));
 
 app.get('*', (req, res, next) => {
-  const markup = renderToString(<App />);
-  res.send(`<!doctype html>
-    <html>
-      <head>
-        <title>SSR demo</title>
-        <script src='/bundle.js' defer></script>
-      </head>
-      <body>
-        <div id='app'>${markup}</div>
-      </body>
-    </html>
-  `);
+  const store = configureStore();
+  const activeRoute = routes.find(route => matchPath(req.url, route)) || {};
+  const promise = activeRoute.fetchInitialData
+    ? activeRoute.fetchInitialData({
+        path: req.path,
+        reduxDispatch: store.dispatch,
+      })
+    : Promise.resolve();
+  promise
+    .then(data => {
+      const context = {};
+      const markup = renderToString(
+        <Provider store={store}>
+          <StaticRouter location={req.url} context={context}>
+            <App />
+          </StaticRouter>
+        </Provider>
+      );
+      res.send(`<!doctype html>
+      <html>
+        <head>
+          <title>SSR demo</title>
+          <script src='/bundle.js' defer></script>
+          <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+        </head>
+        <body>
+          <div id='app'>${markup}</div>
+        </body>
+      </html>
+    `);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(`${err.name}: ${err.message}`);
+    });
   // const context = {};
   // const store = configureStore();
   // const markup = renderToString(
